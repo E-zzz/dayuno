7a51e34e7065d3f35eb1129d9151d340b37195ba
Ezzz <just.2.e.zzz1325@gmail.com>
2021-02-16 21:38:41 -0500

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+#clear the terminal screen
+import os
+os.system('clear')
+# creating strings
+full_name= "Erick DeLeon"
+print (full_name)
+full_name= "Angel"
+print (full_name)
+
+# list
+name= ["Erick", "Angel", "Micbeth"]
+print (name)
+# to call specific name add [] and add number inside bracket starting with 0 is first name
+
+
+fav_pizza = {
+	"john":"pep",
+	"Erick":"Cheese",
+	"Angel": "More Cheese"
+}
+
+print (fav_pizza["Angel"])
